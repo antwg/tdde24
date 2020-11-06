2af27d73048600fc533aba2f64e94f70fec11255
antwe841 <antwe841@su15-110.ad.liu.se>
2020-11-06 14:35:33 +0100

    uppgift6a final

--- student-utveckling/uppgift_6a.py
+++ student-utveckling/uppgift_6a.py
@@ -7,6 +7,11 @@ def exec_program(code):
         if is_statements(statements):
             for statement in statements:
                 check_type(statement)
+        else:
+            print('else')
+            raise SyntaxError("A statement couldn't be interpreted.")
+    else:
+        raise SyntaxError("Can't interpret as a calc program.")
 
 def check_type(statement):
     # if is_assignment(statement):
@@ -27,16 +32,21 @@ def check_type(statement):
 
     if is_selection(statement):
         if condition(selection_condition(statement)):
-            check_type(statement[2])
-            exec_program(['calc'] + statement[3:])
+            if len(statement) == 3:
+                check_type(statement[2])
+            else:
+                check_type(statement[2])
+                exec_program(['calc'] + statement[3:])
         else:
             exec_program(['calc'] + statement[3:])
 
     elif is_output(statement):
         if is_binaryexpr(output_expression(statement)):
             print(binary_expression(statement[-1]))
+        elif isinstance(statement[-1], int) :
+            print(statement[-1])
         else:
-            print(statement)
+            raise SyntaxError('Invalid expression')
 
     # elif is_input(statement):
     #     statement[0] = input()
@@ -52,8 +62,10 @@ def condition(value):
         return (value[0] > value[-1])
     elif value[1] == '<':
         return (value[0] < value[-1])
-    else:
+    elif value[1] == '=':
         return (value[0] == value[-1])
+    else:
+        raise SyntaxError("Not a valid CONDOPER")
 
 def binary_expression(expression):
     if expression[1] == '+':
@@ -65,38 +77,38 @@ def binary_expression(expression):
     elif expression[1] == '/':
         return expression[0] / expression[2]
 
-#            first_statement(statements)
-# calc1 = ['calc', ['print', 2], ['print', 4]]
+"""The tests"""
+# calc1 = ['calc', ['print', 1]]
+# """Tests if output works with a constant"""
 # exec_program(calc1)
 #
-# calc2 = ['calc', ['if', [3, '>', 5], ['print', 2], ['print', 4]]]
+# calc2 = ['calc', ['print', 'a']]
+# """Make sure if it does not work with a non valid constant"""
 # exec_program(calc2)
-
-calc4 = ['calc', ['print', [5, '+', 4]]]
-exec_program(calc4)
-
-"""
-COMMA = ',' ;
-
-PROGRAM = '[', "'calc'", COMMA, STATEMENTS, ']' ;
-
-STATEMENTS = STATEMENT | STATEMENT, COMMA, STATEMENTS ;
-
-STATEMENT = SELECTION | OUTPUT ;
-
-SELECTION = '[', "'if'", COMMA, CONDITION, COMMA, STATEMENT, [COMMA, STATEMENT], ']'
-
-OUTPUT = '[', "'print'", COMMA, EXPRESSION, ']' ;
-
-EXPRESSION = CONSTANT | BINARYEXPR ;
-
-BINARYEXPR = '[', EXPRESSION, COMMA, BINARYOPER, COMMA, EXPRESSION, ']' ;
-
-CONDITION = '[', EXPRESSION, COMMA, CONDOPER, COMMA, EXPRESSION, ']' ;
-
-BINARYOPER = "'+'" | "'-'" | "'*'" | "'/'" ;
-
-CONDOPER = "'<'" | "'>'" | "'='" ;
-
-CONSTANT = ? a Python number ? ;
-"""
+#
+# calc3 = ['calc', ['print', 1], ['print', [5, '+', 4]]]
+# """Test if output works with expressions"""
+# exec_program(calc3)
+#
+# calc4 = ['calc', ['print', 1], ['print', [5, '%', 4]]]
+# """Test when given a non valid BINARYOPER"""
+# exec_program(calc4)
+#
+# calc5 = ['calc', ['if', [4, '<', 6], ['print', 2], ['print', 4]]]
+# """Tests if proram works if statement is True"""
+# exec_program(calc5)
+#
+# calc6 = ['calc', ['if', [4, '!=', 6], ['print', 2]]]
+# """Tests if proram works with non valid condoperator"""
+# exec_program(calc6)
+#
+# calc7 = ['calc', ['if', [4, '>', 6], ['print', 2], ['print', 4]]]
+# """Tests if proram works if statement is False"""
+# exec_program(calc7)
+#
+# calc8 = ['calc', ['if', [4, '>', 6], ['print', 2], 'print', 4]]
+# """Tests if the structure is incorrect"""
+# exec_program(calc8)
+#
+# calc9 = ['calc']
+# exec_program(calc9)
