69ec355804c59383633ad877c21ad492cb28fc72
antwe841 <antwe841@tlhw-5-1.ad.liu.se>
2020-11-13 15:32:29 +0100

    Final version

--- student-utveckling/uppgift_6a.py
+++ student-utveckling/uppgift_6a.py
@@ -12,7 +12,11 @@ def exec_program(code, input_table = {}):
                     variabletable = (check)
                 else:
                     check
-            return variabletable
+
+            if input_table != variabletable:
+                return variabletable
+            else:
+                return input_table
 
         else:
             raise SyntaxError("A statement couldn't be interpreted.")
@@ -51,7 +55,7 @@ def check_type(statement, variabletable):
 
 
 def exec_input(statement, variabletable):
-    variabletable[input_variable(statement)] = int(input('Enter a value for '
+    variabletable[input_variable(statement)] = int(input('Enter value for '
         + input_variable(statement) + ': '))
     return variabletable
 
@@ -75,35 +79,40 @@ def eval_assignment(statement, variabletable):
         raise SyntaxError('Invalid assignment.')
 
 def exec_output(statement, variabletable):
-
     expression = output_expression(statement)
+    print(expression)
     if is_binaryexpr(expression):
-        print(binary_expression(expression))
+        print(binary_expression(expression), end='\n')
     elif isinstance(expression, (int, float)):
-        print(expression)
+        print(expression, end='\n')
     elif expression in variabletable:
-        print(expression, ' = ', variabletable[expression])
-        return(expression, ' = ', variabletable[expression])
-        """Radbryt"""
+        print(expression, '=', variabletable[expression], end='\n')
+
+
     else:
         raise SyntaxError('Invalid expression.')
 
-
 def eval_condition(statement, variabletable):
     if condition(selection_condition(statement), variabletable):
-        if len(statement) == 3:
-            check_type(statement[2], variabletable)
-        else:
-            check_type(statement[2], variabletable)
-            exec_program(['calc'] + statement[3:], variabletable)
+        check_type(statement[2], variabletable)
+        # else:
+        #     check_type(statement[2], variabletable)
+        #     exec_program(['calc'] + statement[3:], variabletable)
+    elif not condition(selection_condition(statement), variabletable)\
+    and len(statement) >= 4:
+        check_type(statement[3], variabletable)
+
+    elif not condition(selection_condition(statement), variabletable):
+        pass
+
     else:
-        exec_program(['calc'] + statement[3:], variabletable)
+        raise SyntaxError(statement)
 
 def condition(value, variabletable):
     statement = value.copy()
     if not isinstance(statement[0], (int, float)):
         if isinstance(statement[0], list):
-            statement[0] = binary_expression(statement[0])
+            statement[0] = binary_expression(statement[0],variabletable)
         elif statement[0] in variabletable:
             statement[0] = variabletable[statement[0]]
         else:
@@ -152,56 +161,30 @@ def binary_expression(expression, variabletable):
         return statement[0] * statement[2]
     elif statement[1] == '/':
         return statement[0] / statement[2]
+    else:
+        raise SyntaxError('Not binary expression')
 
 """The tests"""
-# calc1 = ['calc', ['print', 1]]
-# """Tests if output works with a constant"""
+
+# calc1 = ['calc', 'print', 1]
+# """Tests with invalid statement"""
 # exec_program(calc1)
 #
-# calc2 = ['calc', ['print', 'a']]
-# """Make sure if it does not work with a non valid constant"""
+# calc2 = ['print', 1]
+# """Tests with invalid structure"""
 # exec_program(calc2)
 #
-# calc3 = ['calc', ['print', 1], ['print', [5, '+', 4]]]
-# """Test if output works with expressions"""
+# calc3 = ['calc']
+# """Tests with 0 statements"""
 # exec_program(calc3)
 #
-# calc4 = ['calc', ['print', 1], ['print', [5, '%', 4]]]
-# """Test when given a non valid BINARYOPER"""
+# calc4 = ['calc', ['if', [4, '!=', 6], ['print', 2]]]
+# """"Tests an invalid condoperator"""
 # exec_program(calc4)
 #
-# calc5 = ['calc', ['if', [4, '<', 6], ['print', 2], ['print', 4]]]
-# """Tests if proram works if statement is True"""
+# calc5 = ['calc', ['if', [[5, '%', 4], '<', 6], ['print', 2]]]
+# """Tests an invalid binary operator"""
 # exec_program(calc5)
-#
-# calc6 = ['calc', ['if', [4, '!=', 6], ['print', 2]]]
-# """Tests if proram works with non valid condoperator"""
+# calc6 = ['calc',[ 'set', 'x', 'a']]
+# """Tries to assign non numeric value to variable"""
 # exec_program(calc6)
-#
-# calc7 = ['calc', ['if', [4, '>', 6], ['print', 2], ['print', 4]]]
-# """Tests if proram works if statement is False"""
-# exec_program(calc7)
-#
-# calc8 = ['calc', ['if', [4, '>', 6], ['print', 2], 'print', 4]]
-# """Tests if the structure is incorrect"""
-# exec_program(calc8)
-#
-# calc9 = ['calc']
-# exec_program(calc9)
-#
-# calc10 = ['calc', ['if', [[8, '+', 2], '<', 6], ['print', 2], ['print', 4]]]
-# """Tests if proram works if statement is True"""
-# exec_program(calc10)
-#
-# calc11 = ['calc', ['if', [[3, '+', 2], '<', 'a'], ['print', 2], ['print', 4]]]
-# """Tests if proram works if statement is True"""
-# exec_program(calc11, {'a': 5})
-# calc12 = ['calc', ['set', 'x', 7],['set', 'y', 12], ['set', 'z', ['x', '+', 'y']], ['print', 'z']]
-# print(exec_program(calc12))
-
-
-# calc3 = ['calc', ['read', 'p1'], ['print', 'p1']]
-# exec_program(calc3, {'p0': 3})
-
-calc2 = calc4 = ['calc', ['read', 'n'],['set', 'sum', 0], ['while', ['n', '>', 0], ['set', 'sum', ['sum', '+', 'n']], ['set', 'n', ['n', '-', 1]]], ['print', 'sum']]
-exec_program(calc2)
