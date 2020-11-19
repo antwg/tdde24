fa4d2e2b03b23629166da913fd5e22789f121176
Christopher Wåtz <chrwa634@student.liu.se>
2020-11-19 12:06:20 +0100

    Done it correctly

--- student-utveckling/uppgift_6a.py
+++ student-utveckling/uppgift_6a.py
@@ -1,35 +1,42 @@
 from calc import *
 
-#returnar inte nått värde
 def exec_program(code, input_table = {}):
-
+    """Runs a program"""
     if is_program(code):
         statements = program_statements(code)
         if is_statements(statements):
+            return exec_statements(statements, input_table)
 
-            #creates a copy of the input table so the function is not destructive
-            variabletable = input_table.copy()
-            variabletable = exec_statement(first_statement(statements), variabletable)
-
-            if empty_statements(rest_statements(statements)):
-                if input_table != variabletable:
-                    return variabletable
-                else:
-                    return input_table
-
-            variabletable = exec_program(['calc'] + rest_statements(statements), variabletable)
-
-            #if changes were made in the variable table return the new table
-            if input_table != variabletable:
-                return variabletable
-            else:
-                return input_table
         else:
-
             raise SyntaxError("A statement couldn't be interpreted.")
     else:
 
         raise SyntaxError("Can't interpret as a calc program.")
+
+
+def exec_statements(statements, input_table):
+    #calc1 = ['calc', ['print', 5], ['set', 'a', 5], ['print', 'a']]
+    statement_first = first_statement(statements)
+    statements_rest = rest_statements(statements)
+
+    if is_assignment(statement_first) or is_input(statement_first)\
+    or is_repetition(statement_first):
+        #creates a copy of the input table so the function is not destructive
+        variabletable = input_table.copy()
+        variabletable = exec_statement(statement_first, variabletable)
+
+        if empty_statements(rest_statements(statements)):
+            return variabletable
+
+        return exec_statements(statements_rest, variabletable)
+
+    exec_statement(statement_first, input_table)
+
+    if empty_statements(rest_statements(statements)):
+
+        return input_table
+
+    return exec_statements(rest_statements(statements), input_table)
 
 
 def exec_statement(statement, variabletable):
@@ -44,10 +51,12 @@ def exec_statement(statement, variabletable):
         return exec_condition(statement, variabletable)
 
     elif is_output(statement):
-        return exec_output(statement, variabletable)
+        exec_output(statement, variabletable)
 
     elif is_input(statement):
         return exec_input(statement, variabletable)
+    else:
+        raise SyntaxError('A statement could no be interpreted.')
 
 
 def exec_expression(expression, variabletable):
@@ -63,8 +72,8 @@ def exec_expression(expression, variabletable):
         return variabletable[expression]
 
     else:
-        raise SyntaxError(str(expression) + ": The given expression was not\
-        a valid expression")
+        raise SyntaxError(str(expression) + """: The given expression was not
+        a valid expression""")
 
 
 def exec_input(statement, variabletable):
@@ -95,23 +104,19 @@ def exec_output(statement, variabletable):
     else:
         print(expression)
 
-    return variabletable
-
 
 def exec_condition(statement, variabletable):
     """Checks if condition is met"""
     condition_value = condition(selection_condition(statement), variabletable)
 
     if condition_value:
-        exec_statement(statement[2], variabletable)
-        return variabletable
+        return exec_statement(statement[2], variabletable)
 
     elif not condition_value and len(statement) >= 4:
-        exec_statement(statement[3], variabletable)
-        return variabletable
+        return exec_statement(statement[3], variabletable)
 
     elif not condition_value:
-        return variabletable
+        pass
 
     else:
         raise SyntaxError("In exec condition: invalid condition")
@@ -156,16 +161,15 @@ def exec_repetion(statement, variabletable):
 
     if condition(condition_rep, variabletable):
 
-        for i in range(len(statement_rep)):
+        variabletable = exec_statements(statement_rep, variabletable)
 
-            variabletable = exec_statement(statement_rep[i], variabletable)
-
-        exec_statement(statement, variabletable)
+        return exec_statement(statement, variabletable)
 
     return variabletable
 
 """The tests"""
-
+# calc1 = ['calc', ['read', 'n'], ['set', 'sum', 0], ['while', ['n', '>', 0], ['set', 'sum', ['sum', '+', 'n']], ['set', 'n', ['n', '-', 1]]], ['print', 'sum']]
+# exec_program(calc1)
 # calc1 = ['calc', 'print', 1]
 # """Tests with invalid statement"""
 # exec_program(calc1)
@@ -181,7 +185,7 @@ def exec_repetion(statement, variabletable):
 # calc4 = ['calc', ['if', [4, '!=', 6], ['print', 2]]]
 # """"Tests an invalid condoperator"""
 # exec_program(calc4)
-#
+
 # calc5 = ['calc', ['if', [[5, '%', 4], '<', 6], ['print', 2]]]
 # """Tests an invalid binary operator"""
 # exec_program(calc5)
