01f3ef89e44cc4543916bc3eda1361a9d517c331
Anton <antwe841@student.liu.se>
2020-11-30 14:27:46 +0100

    Created contains key

--- student-utveckling/uppgift7_b.py
+++ student-utveckling/uppgift7_b.py
@@ -6,9 +6,11 @@ def empty_tree_fn():
     return 0
 
 def leaf_fn(key):
+    # print('leaf', key)
     return key**2
 
 def inner_node_fn(key, left_value, right_value):
+    # print('node', key, 'left', left_value, 'right', right_value)
     return key + left_value
 
 #definerad själv
@@ -27,16 +29,40 @@ def return_key(tree):
 def traverse(tree, inner_node_func, leaf_func, empty_tree_func):
 
     if is_empty_tree(tree):
-        empty_tree_func()
+        return empty_tree_func()
 
     elif is_leaf(tree):
         return leaf_func(tree)
 
     elif is_inner_node(tree):
-        left = traverse(left_subtree(tree), inner_node_func, leaf_func, empty_tree_func)
-        right = traverse(left_subtree(tree), inner_node_func, leaf_func, empty_tree_func)
+        left = traverse(left_subtree(tree), inner_node_func, leaf_func,
+                        empty_tree_func)
+        right = traverse(right_subtree(tree), inner_node_func, leaf_func,
+                        empty_tree_func)
 
         return inner_node_func(return_key(tree), left, right)
 
-print(traverse([6, 7, 8], inner_node_fn, leaf_fn, empty_tree_fn))
-print(traverse([[4, 5, 6], 7, [[],8, 9]], inner_node_fn, leaf_fn, empty_tree_fn))
+# print(traverse([6, 7, 8], inner_node_fn, leaf_fn, empty_tree_fn))
+# print(traverse([[4, 5, 6], 7, [[],8, 9]], inner_node_fn, leaf_fn, empty_tree_fn))
+
+
+
+
+def contains_key(key, tree):
+    # print(tree)
+    def inner_node_func(tree, left, right):
+
+        # print('node', tree, 'left', left, 'right', right, 'return',
+        # contains_key(left or right, tree), 'left or right', left or right)
+
+        return left or right
+
+    def leaf_func(tree):
+        # print('leaf', tree,  tree == key)
+        return tree == key
+
+    def empty_tree_func():
+        # print('false')
+        return False
+
+    return traverse(tree, inner_node_func, leaf_func, empty_tree_func)
