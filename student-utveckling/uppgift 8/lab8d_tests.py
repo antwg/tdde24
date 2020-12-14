2265f7a68c2a97f77076112dcfaa20060a81666b
chrwa634 <chrwa634@tlhw-4-3.ad.liu.se>
2020-12-14 08:49:38 +0100

    made the tests for D

--- student-utveckling/uppgift 8/lab8d_tests.py
+++ student-utveckling/uppgift 8/lab8d_tests.py
@@ -21,6 +21,45 @@ def create_tests_for_free_span() -> dict:
     # -------- YOUR TEST CASES GO HERE -----------------------
     # For each case, add a brief description of what you want to test.
 
+    store_test_case(
+    """Test if the entire span is booked"""
+        test_cases,
+        2,
+        start_str="08:00",  # Search interval starts
+        end_str="21:00",  # Search interval ends
+        booking_data=["07:00-21:00"],  # This day's appointments
+        exp_result=[],
+    )
+
+    store_test_case(
+    """Test for no appointments"""
+        test_cases,
+        3,
+        start_str="08:00",  # Search interval starts
+        end_str="21:00",  # Search interval ends
+        booking_data=[],  # This day's appointments
+        exp_result=["08:00-21:00"],
+    )
+
+    store_test_case(
+    """Test with minutes"""
+        test_cases,
+        4,
+        start_str="08:15",  # Search interval starts
+        end_str="21:30",  # Search interval ends
+        booking_data=["07:52-09:32", "13:33-18:27"],  # This day's appointments
+        exp_result=["09:32-13:33", "18:27-21:30"],
+    )
+
+    store_test_case(
+    """Test with appointments directly after each other"""
+        test_cases,
+        5,
+        start_str="08:15",  # Search interval starts
+        end_str="21:30",  # Search interval ends
+        booking_data=["07:52-09:32", "09:32-09:50", "13:33-18:27"],  # This day's appointments
+        exp_result=["09:50-13:33", "18:27-21:30"],
+    )
     print("Test cases generated.")
 
     return test_cases
