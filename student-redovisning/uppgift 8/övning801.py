c62c5144a2ee4f06c6291ffbdee255af3bfe3617
chrwa634 <Christopher.waatz@gmail.com>
2020-12-16 18:13:23 +0100

    finished and moved the lab8 files into redovisning

---
+++ student-redovisning/uppgift 8/övning801.py
@@ -0,0 +1,13 @@
+from cal_ui import *
+
+create('Anton')
+book('Anton', 23, 'apr', '15:00', '17:00', 'Kalas')
+show('Anton', 23, 'apr')
+
+create('Axel')
+book('Axel', 3, 'jan', '12:00', '13:00', 'Simma')
+show('Axel', 3, 'jan')
+
+show_calendars()
+
+save('testdata')
